8674ac154d2de4d57ba22934d95bb644cf7dd7d8
smileyLi <2075245974@qq.com>
2025-07-31 18:53:22 +0800

    fix version1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,5 @@
 <template>
   <router-view />
-
-  <div id="app">
-    <AppHeader />
-    
-    <main class="main-content">
-      <!-- 路由视图容器 -->
-      <router-view />
-    </main>
-    
-    <AppFooter />
-  </div>
 </template>
 
 <script setup>
@@ -24,10 +13,5 @@
     width: 100%;
     height: 100%;
     overflow-x: hidden;
-  }
-
-  .main-content {
-  min-height: calc(100vh - 120px); /* 根据实际页眉页脚高度调整 */
-  padding: 2rem 0;
   }
 </style>
